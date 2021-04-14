c19c825ee0a5d218c7f0d6fe52b7be1929531e49
hjjja <jahj30734@gmail.com>
2021-04-15 00:13:23 +0900

    L2_12_푸는중임

--- src/SolutionL2_12.java
+++ src/SolutionL2_12.java
@@ -4,6 +4,14 @@ public class SolutionL2_12 {
 
     public SolutionL2_12(int[] numbers) {
         this.input = input;
+        String[] num_s=new String[numbers.length];
+        for(int i=0;i<numbers.length;i++) {
+            num_s[i] = Integer.toString(numbers[i]);
+        }
+        //num_s 각각 길이를 통해 몇자리순지 알아내고, 나눠서 맨 앞자리가 큰 애를 찾아.
+        //맨 앞자리가 같은 수라면 두번째 자리 수에 대해 반복해
+
+        //암튼 맨앞자리 수 젤큰애가 맨앞으로 가야함
     }
 
 
